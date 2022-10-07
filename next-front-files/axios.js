3626f9602ad888d3d19d1d6927ed87f92b74c2dd
Jorge Roa <jroaes@gmail.com>
2022-10-07 13:09:12 -0300

    Create axios.js

---
+++ next-front-files/axios.js
@@ -0,0 +1,12 @@
+import Axios from 'axios'
+
+const axios = Axios.create({
+    baseURL: process.env.NEXT_PUBLIC_BACKEND_URL,
+    headers: {
+        'X-Requested-With': 'XMLHttpRequest',
+    },
+    withCredentials: true,
+    credentials: true
+})
+
+export default axios
